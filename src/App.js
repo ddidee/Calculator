89f58bbfdd1741fe7c5394d86b771d6b7a20406a
ddidee <olawaleadeola20@gmail.com>
2022-08-14 13:20:39 +0100

    State components and media queries

--- src/App.js
+++ src/App.js
@@ -1,58 +1,75 @@
-import React, { useState } from 'react'
-// import Classes from './App.module.css';
+import React, {Component} from 'react'
+import './App.css'
 
-const App = () => {
-  const [output, setOutput] = useState('')
+class App extends Component {
+  state ={
+    output: ''
+  }
+  handleDisplay = (e) => {
+    const output = this.state.output;
 
-  const handleDisplay = (e) => {
-    setOutput(output.concat(e.target.value))
+    this.setState({
+      output: output.concat(e.target.value)
+    })
   }
 
-  const handleClear = () => {
-    setOutput('')
+  handleClear = () => {
+    this.setState ({
+      output : ''
+  })
+}
+  handleDelete = () => {
+    const output = this.state.output;
+    
+    this.setState({
+      output: output.slice(0, -1)
+    })
   }
 
-  const handleDelete = () => {
-    setOutput(output.slice(0, -1))
-  }
+  handleCalculate = () => {
+    const output = this.state.output;
 
-  const handleCalculate = () => {
     try{
-      setOutput(eval(output))
+      this.setState({
+        output: (eval(output))
+      })
     }
     catch (err) {
-      setOutput('Error')
+      this.setState({
+        output: ('Error')
+      })
     }
   }
-  return (
-    <div className="App">
-      <form>
-        <input type='text' placeholder="0" value={output}/>
-      </form>
-      <div className='buttons'>
-        <button className='character' onClick={handleClear}>C</button>
-        <button className='character' onClick={handleDelete}>DEL</button>
-        <button className='character'>%</button>
-        <button className='character' value= '/' onClick={handleDisplay}>/</button>
-        <button className='7' value= '7' onClick={handleDisplay}>7</button>
-        <button className='8' value= '8' onClick={handleDisplay}>8</button>
-        <button className='9' value= '9' onClick={handleDisplay}>9</button>
-        <button className='character' value= '-' onClick={handleDisplay}>-</button>
-        <button className='4' value= '4' onClick={handleDisplay}>4</button>
-        <button className='5' value= '5' onClick={handleDisplay}>5</button>
-        <button className='6' value= '6' onClick={handleDisplay}>6</button>
-        <button className='character' value= '*' onClick={handleDisplay}>*</button>
-        <button className='1' value= '1' onClick={handleDisplay}>1</button>
-        <button className='2' value= '2' onClick={handleDisplay}>2</button>
-        <button className='3' value= '3' onClick={handleDisplay}>3</button>
-        <button className='character' value= '+' onClick={handleDisplay}>+</button>
-        <button className='character' value= '.' onClick={handleDisplay}>.</button>
-        <button className='' value= '0' onClick={handleDisplay}>0</button>
-        <button className='character' value= '=' onClick={handleCalculate}>=</button>
-
+  render(){
+    return (
+      <div className="App">
+        <div className='Calculator'>
+          <input className='Input' type='text' placeholder="0" value={this.state.output}/>
+          <div className='Buttons'>
+            <button className='Character' onClick={this.handleClear}>C</button>
+            <button className='Character' onClick={this.handleDelete}>DEL</button>
+            <button className='Character' onClick={this.handleDisplay}>00</button>
+            <button className='Character' value= '/' onClick={this.handleDisplay}>/</button>
+            <button className='Number' value= '7' onClick={this.handleDisplay}>7</button>
+            <button className='Number' value= '8' onClick={this.handleDisplay}>8</button>
+            <button className='Number' value= '9' onClick={this.handleDisplay}>9</button>
+            <button className='Character' value= '-' onClick={this.handleDisplay}>-</button>
+            <button className='Number' value= '4' onClick={this.handleDisplay}>4</button>
+            <button className='Number' value= '5' onClick={this.handleDisplay}>5</button>
+            <button className='Number' value= '6' onClick={this.handleDisplay}>6</button>
+            <button className='Character' value= '*' onClick={this.handleDisplay}>*</button>
+            <button className='Number' value= '1' onClick={this.handleDisplay}>1</button>
+            <button className='Number' value= '2' onClick={this.handleDisplay}>2</button>
+            <button className='Number' value= '3' onClick={this.handleDisplay}>3</button>
+            <button className='Character' value= '+' onClick={this.handleDisplay}>+</button>
+            <button className='Character' value= '.' onClick={this.handleDisplay}>.</button>
+            <button className='Number' value= '0' onClick={this.handleDisplay}>0</button>
+            <button className='CharacterEquals' onClick={this.handleCalculate} value= '='>=</button>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
