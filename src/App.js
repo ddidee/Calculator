07c71a0c3e472a8629dde50b81e902b5f31cbfbd
ddidee <olawaleadeola20@gmail.com>
2022-08-14 13:14:38 +0100

    State components and media queries

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+// import Classes from './App.module.css';
 
-function App() {
+const App = () => {
+  const [output, setOutput] = useState('')
+
+  const handleDisplay = (e) => {
+    setOutput(output.concat(e.target.value))
+  }
+
+  const handleClear = () => {
+    setOutput('')
+  }
+
+  const handleDelete = () => {
+    setOutput(output.slice(0, -1))
+  }
+
+  const handleCalculate = () => {
+    try{
+      setOutput(eval(output))
+    }
+    catch (err) {
+      setOutput('Error')
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input type='text' placeholder="0" value={output}/>
+      </form>
+      <div className='buttons'>
+        <button className='character' onClick={handleClear}>C</button>
+        <button className='character' onClick={handleDelete}>DEL</button>
+        <button className='character'>%</button>
+        <button className='character' value= '/' onClick={handleDisplay}>/</button>
+        <button className='7' value= '7' onClick={handleDisplay}>7</button>
+        <button className='8' value= '8' onClick={handleDisplay}>8</button>
+        <button className='9' value= '9' onClick={handleDisplay}>9</button>
+        <button className='character' value= '-' onClick={handleDisplay}>-</button>
+        <button className='4' value= '4' onClick={handleDisplay}>4</button>
+        <button className='5' value= '5' onClick={handleDisplay}>5</button>
+        <button className='6' value= '6' onClick={handleDisplay}>6</button>
+        <button className='character' value= '*' onClick={handleDisplay}>*</button>
+        <button className='1' value= '1' onClick={handleDisplay}>1</button>
+        <button className='2' value= '2' onClick={handleDisplay}>2</button>
+        <button className='3' value= '3' onClick={handleDisplay}>3</button>
+        <button className='character' value= '+' onClick={handleDisplay}>+</button>
+        <button className='character' value= '.' onClick={handleDisplay}>.</button>
+        <button className='' value= '0' onClick={handleDisplay}>0</button>
+        <button className='character' value= '=' onClick={handleCalculate}>=</button>
+
+      </div>
     </div>
   );
 }
